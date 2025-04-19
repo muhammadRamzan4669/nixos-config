af04f1d0afc712b00b4b5c3d1eb3fa3c126b1320
LYNX <ramzanlynx@icloud.com>
2025-04-19 08:09:45 +0500

    ok

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
                   home-manager.nixosModules.home-manager{
                     home-manager.useGlobalPkgs = true;
                     home-manager.useUserPackages = true;
-                    home-manger.users.lynx = import ./home.nix;
+                    home-manager.users.lynx = import ./home.nix;
                   }
                 ];
 	      };
