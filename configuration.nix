6de678441ff33ad15063b206e6c41cefd3283d53
LYNX <ramzanlynx@icloud.com>
2025-04-19 13:00:50 +0500

    ok bro

--- configuration.nix
+++ configuration.nix
@@ -49,7 +49,7 @@
   
   programs.zsh.enable = true;
   programs.chromium.enable = true;
-  programs.hyprland.enable = true;
+  #programs.hyprland.enable = true;
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
   # services.xserver.xkb.options = "eurosign:e,caps:escape";
@@ -82,6 +82,7 @@
       chromium
       neovim
     ];
+    programs.hyprland.enable = true;
   };
 
   # programs.firefox.enable = true;
