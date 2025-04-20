64426d9dff8c95ee1375cc409adda1a468ae40a3
LYNX <ramzanlynx@icloud.com>
2025-04-20 14:31:12 +0500

    ok

--- home.nix
+++ home.nix
@@ -34,28 +34,6 @@
       right = 1;
     };
   };
-  display = {
-    size = {
-      binaryPrefix = "si";
-    };
-    color = "blue";
-    separator = "  ";
-  };
-  modules = [
-    {
-      type = "datetime";
-      key = "Date";
-      format = "{1}-{3}-{11}";
-    }
-    {
-      type = "datetime";
-      key = "Time";
-      format = "{14}:{17}:{20}";
-    }
-    "break"
-    "player"
-    "media"
-  ];
   };
 
   programs.zsh = {
