a74885aa67553291abf9036475848edb671eeca3
LYNX <ramzanlynx@icloud.com>
2025-04-20 14:28:59 +0500

    ok

--- home.nix
+++ home.nix
@@ -28,33 +28,34 @@
   };
 
   programs.fastfetch = {
-    enable = true;
-
-    settings = {
-	"modules"= [
-	    "title",
-	    "separator",
-	    "os",
-	    "host",
-	    "kernel",
-	    "uptime",
-	    "packages",
-	    "shell",
-	    "terminal",
-	    "cpu",
-	    "gpu",
-	    "memory",
-	    "disk",
-	    "break",
-	    "colors"
-	  ],
-	"logo"= {
-	    "type": "nixos-small",
-	    "width": 34,
-	    "colors": [ "#7EBAE4", "#5277C3", "#FFFFFF" ],
-	    "padding": { "top": 1, "left": 2 }
-	}
+	logo = {
+    source = "nixos_small";
+    padding = {
+      right = 1;
     };
+  };
+  display = {
+    size = {
+      binaryPrefix = "si";
+    };
+    color = "blue";
+    separator = "  ";
+  };
+  modules = [
+    {
+      type = "datetime";
+      key = "Date";
+      format = "{1}-{3}-{11}";
+    }
+    {
+      type = "datetime";
+      key = "Time";
+      format = "{14}:{17}:{20}";
+    }
+    "break"
+    "player"
+    "media"
+  ];
   };
 
   programs.zsh = {
