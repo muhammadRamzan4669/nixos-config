5e22295a2865e9fb57b1ba3e31a187efd1bc8c2d
LYNX <ramzanlynx@icloud.com>
2025-04-20 08:58:00 +0500

    ok

--- home.nix
+++ home.nix
@@ -15,7 +15,23 @@
   ];
 
   # Optional program configurations
-  programs.zsh.enable = true;
+  programs.zsh = {
+  
+  enable = true;
+  plugins = [
+      {
+        name = "zsh-syntax-highlighting";
+        src = pkgs.zsh-syntax-highlighting;
+        file = "share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh";
+      }
+      {
+        name = "zsh-autosuggestions";
+        src = pkgs.zsh-autosuggestions;
+        file = "share/zsh-autosuggestions/zsh-autosuggestions.zsh";
+      }
+    ];
+  };
+
   programs.chromium.enable = true;
   programs.git = {
     enable = true;
