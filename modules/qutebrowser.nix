5e2603c0f43c0d3ee18b1a1f8455583bed01dbf6
LYNX <ramzanlynx@icloud.com>
2025-04-23 17:03:45 +0500

    ok

--- modules/qutebrowser.nix
+++ modules/qutebrowser.nix
@@ -20,38 +20,38 @@
 
     extraConfig = ''
       # Cookie settings
-      config.set('content.cookies.accept', 'all', 'chrome-devtools://*')
-      config.set('content.cookies.accept', 'all', 'devtools://*')
+      config.set('content.cookies.accept', 'all', 'chrome-devtools://*');
+      config.set('content.cookies.accept', 'all', 'devtools://*');
 
       # Accept-Language header
-      config.set('content.headers.accept_language', '', 'https://matchmaker.krunker.io/*')
+      config.set('content.headers.accept_language', '', 'https://matchmaker.krunker.io/*');
 
       # User agent settings
-      config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}) AppleWebKit/{webkit_version} (KHTML, like Gecko) {upstream_browser_key}/{upstream_browser_version} Safari/{webkit_version}', 'https://web.whatsapp.com/')
-      config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}; rv:133.0) Gecko/20100101 Firefox/133.0', 'https://accounts.google.com/*')
+      config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}) AppleWebKit/{webkit_version} (KHTML, like Gecko) {upstream_browser_key}/{upstream_browser_version} Safari/{webkit_version}', 'https://web.whatsapp.com/');
+      config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}; rv:133.0) Gecko/20100101 Firefox/133.0', 'https://accounts.google.com/*');
 
       # Image settings
-      config.set('content.images', True, 'chrome-devtools://*')
-      config.set('content.images', True, 'devtools://*')
+      config.set('content.images', True, 'chrome-devtools://*');
+      config.set('content.images', True, 'devtools://*');
 
       # JavaScript clipboard settings
-      config.set('content.javascript.clipboard', 'access-paste', 'https://chatgpt.com')
-      config.set('content.javascript.clipboard', 'access-paste', 'https://github.com')
-      config.set('content.javascript.clipboard', 'access-paste', 'https://grok.com')
-      config.set('content.javascript.clipboard', 'access-paste', 'https://replit.com')
+      config.set('content.javascript.clipboard', 'access-paste', 'https://chatgpt.com');
+      config.set('content.javascript.clipboard', 'access-paste', 'https://github.com');
+      config.set('content.javascript.clipboard', 'access-paste', 'https://grok.com');
+      config.set('content.javascript.clipboard', 'access-paste', 'https://replit.com');
 
       # JavaScript enabled settings
-      config.set('content.javascript.enabled', True, 'chrome-devtools://*')
-      config.set('content.javascript.enabled', True, 'devtools://*')
-      config.set('content.javascript.enabled', True, 'chrome://*/*')
-      config.set('content.javascript.enabled', True, 'qute://*/*')
+      config.set('content.javascript.enabled', True, 'chrome-devtools://*');
+      config.set('content.javascript.enabled', True, 'devtools://*');
+      config.set('content.javascript.enabled', True, 'chrome://*/*');
+      config.set('content.javascript.enabled', True, 'qute://*/*');
 
       # Local content access settings
-      config.set('content.local_content_can_access_remote_urls', True, 'file:///home/lynx/.local/share/qutebrowser/userscripts/*')
-      config.set('content.local_content_can_access_file_urls', False, 'file:///home/lynx/.local/share/qutebrowser/userscripts/*')
+      config.set('content.local_content_can_access_remote_urls', True, 'file:///home/lynx/.local/share/qutebrowser/userscripts/*');
+      config.set('content.local_content_can_access_file_urls', False, 'file:///home/lynx/.local/share/qutebrowser/userscripts/*');
 
       # Notification settings
-      config.set('content.notifications.enabled', False, 'https://www.youtube.com')
+      config.set('content.notifications.enabled', False, 'https://www.youtube.com');
     '';
   };
 }
