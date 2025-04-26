9eed117ade9324c73d63fec215c504f246e2ba8c
LYNX <ramzanlynx@icloud.com>
2025-04-26 20:06:16 +0500

    ok

--- modules/ghostty.nix
+++ modules/ghostty.nix
@@ -8,6 +8,9 @@
       theme = "catppuccin-mocha";
       font-size = 24;
       confirm-close-surface = false;
+      cursor-style = block;
+      mouse-hide-while-typing = true;
+      window-padding-x = 2,2;
     };
   };
 }
