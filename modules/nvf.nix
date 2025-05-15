2a2580323568954e6716ad8889770f4bfb25f6ed
LYNX <ramzanlynx@icloud.com>
2025-05-15 17:12:35 +0500

    ok

--- modules/nvf.nix
+++ modules/nvf.nix
@@ -3,14 +3,15 @@
     enable = true;
     settings = {
       vim = {
-        viAlias = false;
-        vimAlias = false;
-        lsp.enable = true;
         theme = {
                 enable=true;
                 name="gruvbox";
                 style="dark";
-        }
+        };
+        languages = {
+                enableLSP = true;
+                enableTreesitter = true;
+        };
       };
     };
   };
