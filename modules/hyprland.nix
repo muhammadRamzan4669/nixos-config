50686e55d1be32d5d8909d6b4f7964bebeb4f7cc
LYNX <ramzanlynx@icloud.com>
2025-05-16 22:12:07 +0500

    ok

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -144,10 +144,10 @@
 	#"$mod SHIFT, G, exec, ~/nix-config/scripts/adjust_gaps.sh decrease"
 	#"$mod, H, exec, ~/nix-config/scripts/adjust_rounding.sh increase"
 	#"$mod SHIFT, H, exec, ~/nix-config/scripts/adjust_rounding.sh decrease"
-        "$mod, equal, exec, sh ~/nix-config/scripts/gaps.sh --inc_gaps_in"
-        "$mod, minus, exec, sh ~/nix-config/scripts/gaps.sh --dec_gaps_in"
-        "$mod, plus, exec, sh ~/nix-config/scripts/gaps.sh --dec_gaps_out"
-        "$mod, underscore, exec, sh ~/nix-config/scripts/gaps.sh --dec_gaps_out"
+        "$mod, bracketleft, exec, sh ~/nix-config/scripts/gaps.sh --inc_gaps_in"
+        "$mod, bracketright, exec, sh ~/nix-config/scripts/gaps.sh --dec_gaps_in"
+        "$mod, equal, exec, sh ~/nix-config/scripts/gaps.sh --dec_gaps_out"
+        "$mod, minus, exec, sh ~/nix-config/scripts/gaps.sh --dec_gaps_out"
       ];
 
       bindm = [
