493e5f6b8e48a54409ccea31e6f5567567e30174
LYNX <ramzanlynx@icloud.com>
2025-05-17 09:37:04 +0500

    ok

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -20,7 +20,7 @@
       general = {
         gaps_in = 2;
         gaps_out = 10;
-        border_size = 1;
+        border_size = 3;
         #"col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
         "col.active_border"="rgba(ff0000ee)";
         "col.inactive_border" = "rgba(595959aa)";
@@ -48,32 +48,43 @@
       };
 
       animations = {
-        enabled = false;
-        bezier = [
-          "easeOutQuint,0.23,1,0.32,1"
-          "easeInOutCubic,0.65,0.05,0.36,1"
-          "linear,0,0,1,1"
-          "almostLinear,0.5,0.5,0.75,1.0"
-          "quick,0.15,0,0.1,1"
-        ];
-        animation = [
-          "global, 1, 10, default"
-          "border, 1, 5.39, easeOutQuint"
-          "windows, 1, 4.79, easeOutQuint"
-          "windowsIn, 1, 4.1, easeOutQuint, popin 87%"
-          "windowsOut, 1, 1.49, linear, popin 87%"
-          "fadeIn, 1, 1.73, almostLinear"
-          "fadeOut, 1, 1.46, almostLinear"
-          "fade, 1, 3.03, quick"
-          "layers, 1, 3.81, easeOutQuint"
-          "layersIn, 1, 4, easeOutQuint, fade"
-          "layersOut, 1, 1.5, linear, fade"
-          "fadeLayersIn, 1, 1.79, almostLinear"
-          "fadeLayersOut, 1, 1.39, almostLinear"
-          "workspaces, 1, 1.94, almostLinear, fade"
-          "workspacesIn, 1, 1.21, almostLinear, fade"
-          "workspacesOut, 1, 1.94, almostLinear, fade"
-        ];
+        enabled = "yes";
+          bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+          animation = [
+            "windows, 1, 4, myBezier"
+            "windowsOut, 1, 4, default, popin 80%"
+            "border, 1, 3, default"
+            "fade, 1, 4, default"
+            "workspaces, 1, 4, default"
+          ];
+
+
+        #enabled = false;
+        #bezier = [
+        #  "easeOutQuint,0.23,1,0.32,1"
+        #  "easeInOutCubic,0.65,0.05,0.36,1"
+        #  "linear,0,0,1,1"
+        #  "almostLinear,0.5,0.5,0.75,1.0"
+        #  "quick,0.15,0,0.1,1"
+        #];
+        #animation = [
+        #  "global, 1, 10, default"
+        #  "border, 1, 5.39, easeOutQuint"
+        #  "windows, 1, 4.79, easeOutQuint"
+        #  "windowsIn, 1, 4.1, easeOutQuint, popin 87%"
+        #  "windowsOut, 1, 1.49, linear, popin 87%"
+        #  "fadeIn, 1, 1.73, almostLinear"
+        #  "fadeOut, 1, 1.46, almostLinear"
+        #  "fade, 1, 3.03, quick"
+        #  "layers, 1, 3.81, easeOutQuint"
+        #  "layersIn, 1, 4, easeOutQuint, fade"
+        #  "layersOut, 1, 1.5, linear, fade"
+        #  "fadeLayersIn, 1, 1.79, almostLinear"
+        #  "fadeLayersOut, 1, 1.39, almostLinear"
+        #  "workspaces, 1, 1.94, almostLinear, fade"
+        #  "workspacesIn, 1, 1.21, almostLinear, fade"
+        #  "workspacesOut, 1, 1.94, almostLinear, fade"
+        #];
       };
 
       dwindle = {
